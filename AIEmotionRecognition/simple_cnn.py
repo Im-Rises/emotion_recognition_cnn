154df352850080a21d9af9093e5f4482b6e5ccb8
QuentinM <59691442+Im-Rises@users.noreply.github.com>
2022-04-28 10:22:40 +0200

    Added model save

--- AIEmotionRecognition/simple_cnn.py
+++ AIEmotionRecognition/simple_cnn.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
+from keras.models import load_model
 from tensorflow.keras.layers import Dense, Conv2D, Input, Flatten, Dropout, MaxPooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping
@@ -25,10 +26,10 @@ if __name__ == '__main__':
     # Train and test data shape
     print("Données entrainement: {}, Test: {}".format(X_train.shape, X_test.shape))
 
-    # Plot an image from the dataset test
-    plt.imshow(X_train[0])
-    plt.title("Tag {}".format(y_train[0]))
-    plt.show()
+    # # Plot an image from the dataset test
+    # plt.imshow(X_train[0])
+    # plt.title("Tag {}".format(y_train[0]))
+    # plt.show()
 
     # We need an image in grayscale, for this purpose we delete the 2 of the layer of the images (RGB => Grayscale).
     # Resizing images function (from numpy)
@@ -38,50 +39,65 @@ if __name__ == '__main__':
     X_train = X_train.reshape(60000, 28, 28, 1)
     X_test = X_test.reshape(10000, 28, 28, 1)
 
-    # CNN
-    cnn = tf.keras.Sequential()
+    saved = False;
+    try:
+        cnn = load_model("savedModel")
+        saved = True
+    except:
+        saved = False
 
-    # 3 convolution layers with progressive filter 32, 64 and 128
-    cnn.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
-    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+    if saved:
+        # Predict test
+        img = X_train[0]
+        print("Image 0 prediction : {}".format(np.argmax(cnn.predict(img.reshape(1, 28, 28, 1)), axis=-1)[0]))
+    else:
+        # CNN
+        cnn = tf.keras.Sequential()
 
-    cnn.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
-    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+        # 3 convolution layers with progressive filter 32, 64 and 128
+        cnn.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
+        cnn.add(MaxPooling2D(pool_size=(2, 2)))
 
-    cnn.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
-    cnn.add(MaxPooling2D(pool_size=(2, 2)))
+        cnn.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
+        cnn.add(MaxPooling2D(pool_size=(2, 2)))
 
-    # Flatten data
-    cnn.add(Flatten())
+        cnn.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=(28, 28, 1), activation='relu'))
+        cnn.add(MaxPooling2D(pool_size=(2, 2)))
 
-    # ANN classic layer
-    cnn.add(Dense(512, activation='relu'))
+        # Flatten data
+        cnn.add(Flatten())
 
-    # Output layer (from 0 to 9)
-    cnn.add(Dense(10, activation='softmax'))
+        # ANN classic layer
+        cnn.add(Dense(512, activation='relu'))
 
-    # earlyStopping to know the number of epoch to do.
-    early_stop = EarlyStopping(monitor='val_loss', patience=2)
+        # Output layer (from 0 to 9)
+        cnn.add(Dense(10, activation='softmax'))
 
-    # Compilate CNN
-    cnn.compile(optimizer='adam',
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
-    cnn.summary()
+        # earlyStopping to know the number of epoch to do.
+        early_stop = EarlyStopping(monitor='val_loss', patience=2)
 
-    # Training
-    cnn.fit(x=X_train,
-            y=y_train,
-            validation_data=(X_test, y_test),
-            epochs=25,
-            callbacks=[early_stop])
+        # Compilate CNN
+        cnn.compile(optimizer='adam',
+                    loss='sparse_categorical_crossentropy',
+                    metrics=['accuracy'])
+        cnn.summary()
 
-    # Print accuracy and losses
-    losses = pd.DataFrame(cnn.history.history)
-    losses[['accuracy', 'val_accuracy']].plot()
+        # Training
+        cnn.fit(x=X_train,
+                y=y_train,
+                validation_data=(X_test, y_test),
+                epochs=25,
+                callbacks=[early_stop])
 
-    losses[['loss', 'val_loss']].plot()
+        # Print accuracy and losses
+        losses = pd.DataFrame(cnn.history.history)
+        losses[['accuracy', 'val_accuracy']].plot()
 
-    # Predict test
-    img = X_train[0]
-    print("Image 0 prediction : {}".format(np.argmax(cnn.predict(img.reshape(1, 28, 28, 1)), axis=-1)[0]))
+        losses[['loss', 'val_loss']].plot()
+
+        # Predict test
+        img = X_train[0]
+        print("Image 0 prediction : {}".format(np.argmax(cnn.predict(img.reshape(1, 28, 28, 1)), axis=-1)[0]))
+
+        # Save last model
+        cnn.save("savedModel")
