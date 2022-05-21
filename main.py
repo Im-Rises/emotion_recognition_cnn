ed3f9d536d15c0052774b439dbbcdda12d6f2da4
clementreiffers <reiffersclement@gmail.com>
2022-05-21 22:00:44 +0100

    detection de la tête dans une vidéo streamée sur un site web

--- main.py
+++ main.py
@@ -1,44 +1,77 @@
-# -*- coding: utf-8 -*-
-
-from tkinter import *
-from PIL import Image, ImageTk
 import cv2
-from emotion_recognition.prediction import camera_modified
+from flask import Flask, render_template, Response, request
+from keras.models import load_model
+
+from emotion_recognition.prediction import get_face_from_frame
+
+switch, out, capture, rec_frame = (
+    1,
+    0,
+    0,
+    0,
+)
+
+face_shape = (80, 80)
+model = load_model("./emotion_recognition/Models/trained_models/resnet50")
+class_cascade = cv2.CascadeClassifier(
+    "./emotion_recognition/ClassifierForOpenCV/frontalface_default.xml"
+)
+
+# instatiate flask app
+app = Flask(__name__, template_folder="./templates")
+
+camera = cv2.VideoCapture(0)
 
 
-# Define function to show frame
-def show_frames():
-    # Get the latest frame and convert into Image
-    cv2image = cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(cv2image)
-    # Convert image to PhotoImage
-    imgtk = ImageTk.PhotoImage(image=img)
-    label.imgtk = imgtk
-    label.configure(image=imgtk)
-    # Repeat after an interval to capture continiously
-    label.after(20, show_frames)
+def gen_frames():  # generate frame by frame from camera
+    while True:
+        success, frame = camera.read()
+        if success:
+            frame, face = get_face_from_frame(
+                cv2.flip(frame, 1), face_shape, class_cascade=class_cascade
+            )
+            try:
+                ret, buffer = cv2.imencode(".jpg", cv2.flip(frame, 1))
+                frame = buffer.tobytes()
+                yield (
+                    b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+                )
+            except Exception as e:
+                pass
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/video_feed")
+def video_feed():
+    return Response(
+        gen_frames(),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
+
+
+@app.route("/requests", methods=["POST", "GET"])
+def tasks():
+    global switch, camera
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        if request.form.get("stop") == "Stop/Start":
+            if switch == 1:
+                switch = 0
+                camera.release()
+                cv2.destroyAllWindows()
+            else:
+                camera = cv2.VideoCapture(0)
+                switch = 1
+
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    # face_shape = (244, 244)
-    #
-    # for frame, emotion in camera_modified(face_shape):
-    #     # update all the interface here
-    #     cv2.imshow("frame", frame)
-    #     # the "emotion" array is a sorted array of all emotions with their probabilities
-    #     if emotion is not None:
-    #         print(emotion)
-
-    # Create an instance of TKinter Window or frame
-    win = Tk()
-
-    # Set the size of the window
-    win.geometry("1000x600")
-
-    # Create a Label to capture the Video frames
-    label = Label(win)
-    label.grid(row=0, column=0)
-    cap = cv2.VideoCapture(0)
-
-    show_frames()
-    win.mainloop()
+    app.run()
+    camera.release()
+    cv2.destroyAllWindows()
